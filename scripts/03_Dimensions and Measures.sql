c7b478e609b8fa9134d1d2e62e8025e59d4cab58
Swayam-Jevoor <227309978+Swayam-Jevoor@users.noreply.github.com>
2025-09-20 17:02:02 +0530

    Update 03_Dimensions and Measures.sql

--- scripts/03_Dimensions and Measures.sql
+++ scripts/03_Dimensions and Measures.sql
@@ -1,7 +1,7 @@
 -- This is a dimension, as the output shows the values which aren't numericSELECT DISTINCT
 
 category
-FROM gold.dim_products
+FROM gold.dim_products;
 
 
 -- This is a measure
@@ -9,35 +9,36 @@ FROM gold.dim_products
 -- and it makes sense to aggregate those values
 SELECT DISTINCT
 sales_amount
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- This is a dimension
 SELECT DISTINCT
 product_name
-FROM gold.dim_products
+FROM gold.dim_products;
 
 
 -- This is a measure
 SELECT DISTINCT
 quantity
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- This is a dimension
 SELECT DISTINCT
 birthdate
-FROM gold.dim_customers
+FROM gold.dim_customers;
 
 
 -- Calculating age from birth date
 -- This is a measure
 SELECT DISTINCT
 DATEDIFF(year, birthdate, GETDATE()) AS Age
-FROM gold.dim_customers
+FROM gold.dim_customers;
 
 
 -- This is a dimension
 SELECT DISTINCT
 customer_id
+
 FROM gold.dim_customers
