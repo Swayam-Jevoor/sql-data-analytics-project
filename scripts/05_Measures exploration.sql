7ac2c27c7ce48c16b66d336a1b30981814d69466
Swayam-Jevoor <227309978+Swayam-Jevoor@users.noreply.github.com>
2025-09-20 17:03:01 +0530

    Update 05_Measures exploration.sql

--- scripts/05_Measures exploration.sql
+++ scripts/05_Measures exploration.sql
@@ -1,58 +1,71 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
 -- Find the total sales
 
 SELECT
 	SUM(sales_amount) AS total_sales
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- Find how many items are sold
 
 SELECT
 	SUM(quantity) AS items_sold
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- Find the average selling price
 
 SELECT
 	AVG(price) AS avg_price
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- Find the total number of orders
 
 SELECT
 	COUNT(order_number) AS total_orders
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 SELECT
 	COUNT(DISTINCT order_number) AS total_orders
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- Find the total number of products
 
 SELECT
 	COUNT(product_name) AS total_products
-FROM gold.dim_products
+FROM gold.dim_products;
 
 SELECT
 	COUNT(DISTINCT product_name) AS total_products
-FROM gold.dim_products
+FROM gold.dim_products;
 
 
 -- Find the total number of customers
 
 SELECT
 	COUNT(customer_key) AS total_customers
-FROM gold.dim_customers
+FROM gold.dim_customers;
 
 
 -- Find the total number of customers that has placed an order
 
 SELECT
 	COUNT(DISTINCT customer_key) AS total_customers
-FROM gold.fact_sales
+FROM gold.fact_sales;
 
 
 -- Generate report that shows all key metrics of the business
@@ -85,4 +98,4 @@ UNION ALL
 SELECT
 	'Total Nr. Customers' AS measure_name,
 	COUNT(customer_key) AS measure_value
-FROM gold.dim_customers
+FROM gold.dim_customers;
