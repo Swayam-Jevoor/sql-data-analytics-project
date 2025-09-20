1f2119119ccd09d319bdf81b77ecbd8d2ca8d8c3
Swayam-Jevoor <227309978+Swayam-Jevoor@users.noreply.github.com>
2025-09-20 17:14:19 +0530

    Add files via upload

---
+++ scripts/09_Change over time analysis.sql
@@ -0,0 +1,53 @@
+-- Change over time analysis
+-- Analyze how a measure evolves over time
+-- Helps track trends and identify seasonality in your data
+
+
+-- Analyze sales performance over time
+
+SELECT
+	YEAR(order_date) AS order_year,
+	MONTH(order_date) AS order_month,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date) ASC
+
+
+-- DATETRUNC() rounds a date or timestamp to a specified date part
+
+SELECT
+	DATETRUNC(year, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(year, order_date)
+ORDER BY DATETRUNC(year, order_date) ASC
+
+
+-- FORMAT() shows the date in the way you give the format of the date
+
+SELECT
+	FORMAT(order_date, 'yyyy-MMM') AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM') 
+
+
+-- How many customers were added each year?
+
+SELECT
+	DATETRUNC(year, create_date) AS create_year,
+	COUNT(customer_key) AS total_customer
+FROM gold.dim_customers
+GROUP BY DATETRUNC(year, create_date)
+ORDER BY DATETRUNC(year, create_date)
